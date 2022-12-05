af52fb10d5efc6f536edf2220cb033d3d9c0b019
Subramanyarao11 <subramanya11rao@gmail.com>
2022-12-05 23:55:05 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Header from "./components/Header"
+import Container from "./components/Container";
+import './App.css'
+
+
+const siteProps = {
+    name: "Subramanya_rao",
+    email: "subramanya11rao@gmail.com",
+    gitHub: "Subramanyarao11",
+    linkedIn: "subramanya-m-rao-67b935213",
+    instagram: "ig_subramnya11",
+    twitter: "Subramanya11rao",
+  };
+
+function App() {
+
+    return (
+        <div className="main-container">
+            <Header />
+            <Container {...siteProps}/>
+        </div>
+
+
+    )
+}
+
+export default App
---
+++ src/components/Container.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import '../index.css'
+// Add your image to assets folder and import it here!
+// import img from "../assets/img.jpg"
+
+export default function Container(props) {
+    const { email, instagram, twitter , linkedIn, github ,name} = props;
+    return (
+        <div className='container'>
+            <div className='image-container'>
+                <img src="https://avatars.githubusercontent.com/u/90202645?v=4" alt={`${name}'s avatar`}/>
+            </div>
+            <h1>@{name}</h1>
+
+
+            {/* Links Tiles */}
+            {/* Email */}
+            <a className="tile" href={`mailto:${email}`} target="_blank">
+                <div className="icon">
+                    {/* Social Media Icon :- From font awesome icons */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" fill="#94a3b8"></path> </svg>
+                </div>
+                <p>Email</p>
+                {/* Share Icon From :- Linktree svg  */}
+                <div className="tile-share-btn">
+                <svg width="16" height="16" viewBox="0 0 16 16" enable-background="new 0 0 24 24" class="sc-gKsewC iPWGYb"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z" fill="white"></path></svg>
+                </div>
+            </a>
+            {/* Instagram */}
+            <a className="tile" href={`https://www.instagram.com/${instagram}`} target="_blank">
+                <div className="icon">
+                    {/* Social Media Icon :- From font awesome icons */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" fill="#94a3b8"></path> </svg>
+                </div>
+                <p>Instagram</p>
+                {/* Share Icon From :- Linktree svg  */}
+                <div className="tile-share-btn">
+                <svg width="16" height="16" viewBox="0 0 16 16" enable-background="new 0 0 24 24" class="sc-gKsewC iPWGYb"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z" fill="white"></path></svg>
+                </div>
+            </a>
+            {/* Twitter */}
+            <a className="tile" href={`https://twitter.com/${twitter}`} target="_blank">
+                <div className="icon">
+                    {/* Social Media Icon :- From font awesome icons */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" fill="#94a3b8"></path> </svg>
+                </div>
+                <p>Twitter</p>
+                {/* Share Icon From :- Linktree svg  */}
+                <div className="tile-share-btn">
+                <svg width="16" height="16" viewBox="0 0 16 16" enable-background="new 0 0 24 24" class="sc-gKsewC iPWGYb"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z" fill="white"></path></svg>
+                </div>
+            </a>
+            {/* Linkedin */}
+            <a className="tile" href={`https://www.linkedin.com/in/${linkedIn}`} target="_blank">
+                <div className="icon">
+                    {/* Social Media Icon :- From font awesome icons */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" fill="#94a3b8"></path> </svg>
+                </div>
+                <p>Linkedin</p>
+                {/* Share Icon From :- Linktree svg  */}
+                <div className="tile-share-btn">
+                <svg width="16" height="16" viewBox="0 0 16 16" enable-background="new 0 0 24 24" class="sc-gKsewC iPWGYb"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z" fill="white"></path></svg>
+                </div>
+            </a>
+            {/* Github */}
+            <a className="tile" href={`https://github.com/${github}`} target="_blank">
+                <div className="icon">
+                    {/* Social Media Icon :- From font awesome icons */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" fill="#94a3b8"></path> </svg>
+                </div>
+                <p>Github</p>
+                {/* Share Icon From :- Linktree svg  */}
+                <div className="tile-share-btn">
+                <svg width="16" height="16" viewBox="0 0 16 16" enable-background="new 0 0 24 24" class="sc-gKsewC iPWGYb"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z" fill="white"></path></svg>
+                </div>
+            </a>
+
+        </div>
+
+    )
+}
+
+Container.propTypes = {
+    name: PropTypes.string.isRequired,
+}
